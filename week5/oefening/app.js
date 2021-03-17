9dc03141d68a3ae258f069c01b73a826adb7eb4b
Indra Doerga <0957822@hr.nl>
2021-03-17 15:11:26 +0100

    prediction en train

--- week5/oefening/app.js
+++ week5/oefening/app.js
@@ -5,13 +5,15 @@ let nn
 let fakeData = createFakedata()
 let plot
 
+console.log(fakeData);
+
 //
 // teken de scatterplot voor de fake data
 //
 async function drawScatterPlot() {
     plot = new VegaScatterplot()
     // await plot.initialise("horsepower", "mpg", 600, 400, fakeData)
-    
+
 }
 
 //
@@ -19,15 +21,29 @@ async function drawScatterPlot() {
 //
 async function createNeuralNetwork() {
     // maak neural network
+    const options = { task: 'regression', debug: true }
+    const nn = ml5.neuralNetwork(options)
 
+    // shuffle
+    fakeData.sort(() => (Math.random() - 0.5))
+
+    // een voor een de training data toevoegen aan het neural network
+    for (let car of fakeData) {
+        nn.addData({ horsepower: car.horsepower }, { mpg: car.mpg })
+    }
 
     // voeg data toe aan neural network met addData
     for (let row of fakeData) {
         // nn.addData({ horsepower: row.horsepower }, { mpg: row.mpg })
     }
 
-    // train neural network
+    // train + normalize neural network
+    nn.normalizeData()
+    nn.train({ epochs: 10 }, () => finishedTraining())
 
+    function finishedTraining() {
+        console.log("Finished training!")
+    }
 }
 
 
@@ -38,11 +54,22 @@ async function trainingFinished() {
     // doe een enkele voorspelling om te zien of alles werkt
     let testCar = { horsepower: 220 }
 
+    async function trainingFinished() {
+    let testCar = { horsepower: 90 }
+
+    const results = await nn.predict(testCar)
+    console.log(results)
+
+    const prediction = results[0].value
+    console.log(`Deze auto zal een verbruik hebben van: ${prediction}`)
+}
+
+
 
 
     // maak een voorspelling voor elk punt op de X as
     let predictions = []
-    for(let i = 0; i< 400; i++) {
+    for (let i = 0; i < 400; i++) {
         // let prediction = ....
         // predictions.push(...)
     }
@@ -52,5 +79,6 @@ async function trainingFinished() {
     // ...
 }
 
-
-setup()
+// start de applicatie
+// drawScatterPlot()
+createNeuralNetwork()
